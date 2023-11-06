e6b5cdcd613ff88ae6803b0eeaa4357dc8f60125
iluvdata <gbellsworth@gmail.com>
2023-11-06 09:34:38 -0500

    reformat

--- R/plumberWebSocket.R
+++ R/plumberWebSocket.R
@@ -7,39 +7,39 @@ library(plumber)
 #' \link[plumber]{Plumber} wrapper to support Websockets
 #' @export
 PlumberWebSocket <- R6Class("PlumberWebSocket", 
-                            inherit = Plumber,
-                            public = list(
-                              #' @description
-                              #' see \link[plumber]{Plumber}
-                              #' 
-                              #' @param f location of file to be plumbed
-                              #' @param wd working directory to launch in and override Plumber's default (where the script resides)
-                              #' 
-                              #' This wrapper is used to correct the working directory
-                              initialize = function(f, wd = getwd()) {
-                                file.copy(f, paste0(wd, "/.plumber.R"))
-                                super$initialize(paste0(wd, "/.plumber.R"))
-                              },
-                              #' @description
-                              #' event fired when websocket is opened
-                              #' 
-                              #' @param ws websocket
-                              onWSOpen = function(ws) {
-                                if (is.function(private$ws_open)) {
-                                  private$ws_open(ws)
-                                }
-                                invisible(self)
-                              },
-                              #' @description
-                              #' assign a websocket
-                              #' 
-                              #' @param open new websocket
-                              websocket = function(open = NULL) {
-                                if (!is.null(open)) stopifnot(is.function(open))
-                                private$ws_open <- open
-                              }
-                            ), 
-                            private = list(
-                              ws_open = NULL
-                            )
+  inherit = Plumber,
+  public = list(
+    #' @description
+    #' see \link[plumber]{Plumber}
+    #' 
+    #' @param f location of file to be plumbed
+    #' @param wd working directory to launch in and override Plumber's default (where the script resides)
+    #' 
+    #' This wrapper is used to correct the working directory
+    initialize = function(f, wd = getwd()) {
+      file.copy(f, paste0(wd, "/.plumber.R"))
+      super$initialize(paste0(wd, "/.plumber.R"))
+    },
+    #' @description
+    #' event fired when websocket is opened
+    #' 
+    #' @param ws websocket
+    onWSOpen = function(ws) {
+      if (is.function(private$ws_open)) {
+        private$ws_open(ws)
+      }
+      invisible(self)
+    },
+    #' @description
+    #' assign a websocket
+    #' 
+    #' @param open new websocket
+    websocket = function(open = NULL) {
+      if (!is.null(open)) stopifnot(is.function(open))
+      private$ws_open <- open
+    }
+  ), 
+  private = list(
+    ws_open = NULL
+  )
 )
